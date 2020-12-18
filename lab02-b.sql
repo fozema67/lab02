546d9a26ffb71a12b99d8c273711fed11674cccf
Дмитрий <fozema67@gmail.com>
2020-12-18 21:54:46 +0300

    Update lab02-b.sql

--- lab02-b.sql
+++ lab02-b.sql
@@ -1,8 +1,8 @@
 alter table soft add constraint del_soft_id_pri primary key (id);
---alter table installs add constraint del_pc_id_for foreign key(pc_id) references pcs (id);
+alter table installs add constraint del_pc_id_for foreign key(pc_id) references pcs (id);
 alter table installs add constraint del_soft_id_for foreign key(soft_id) references soft (id);
 alter table pcs add constraint del_pcs_id_pri primary key (id);
---alter table pcs add constraint del_room_id_for foreign key(room_id) references rooms (id);
+alter table pcs add constraint del_room_id_for foreign key(room_id) references rooms (id);
 alter table rooms add constraint del_rooms_id_pri primary key (id);
---alter table rooms add constraint del_rooms_id_for foreign key(emp_id) references employees (id);
+alter table rooms add constraint del_rooms_id_for foreign key(emp_id) references employees (id);
 alter table employees add constraint del_empl_id_pri primary key (id);
